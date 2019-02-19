63f415d47b1417c3d96841b3d9ff4ea945057d04
David Cohen <dcohen@myc.dfci.harvard.edu>
2019-02-19 11:36:12 -0500

    Fixing log paths

--- rnaseq_align.py
+++ rnaseq_align.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 '''
-RNA-Seq Alignment Workflow Slurm Script Generator
-Generates and submits a Slurm script that takes a BAM file, converts it to FASTQ, uses STAR two-pass method via ICGC code, and produces an aligned BAM file
+RNA-Seq Alignment Workflow SLURM Script Generator
+Generates and submits a SLURM script that takes a BAM file, converts it to FASTQ, uses STAR two-pass method via ICGC code, and produces an aligned BAM file
 David Cohen, February 2019
 https://docs.gdc.cancer.gov/Data/Bioinformatics_Pipelines/Expression_mRNA_Pipeline/
 '''
@@ -10,7 +10,6 @@ https://docs.gdc.cancer.gov/Data/Bioinformatics_Pipelines/Expression_mRNA_Pipeli
 import argparse
 import os
 import sys
-
 
 parser = argparse.ArgumentParser(description="RNA-Seq Alignment Workflow SLURM Submission File Generator", formatter_class=argparse.ArgumentDefaultsHelpFormatter, usage='%(prog)s [options]', add_help=True)
 required = parser.add_argument_group("required input parameters")
@@ -38,7 +37,7 @@ if workDir.endswith("/"):
 
 job_file = workDir + "/Slurm/" + outName + "_align.sh"
 
-# Write Slurm script
+# Write SLURM script
 
 with open(job_file,"w") as fh:
 
@@ -81,16 +80,16 @@ with open(job_file,"w") as fh:
         fh.writelines("\n")
         fh.writelines("# Prepare FASTQ for star_align.py\n")
         fh.writelines("\n")
-        fh.writelines("tar cvf /" + workDir + "/" + outName + "/" + outName + "_TEMP_fastq.tar \\\n")
-        fh.writelines("/" + workDir + "/" + outName + "/" + outName + "_1.fastq.gz \\\n")
-        fh.writelines("/" + workDir + "/" + outName + "/" + outName + "_2.fastq.gz \\\n")
-        fh.writelines("--remove-files\n")
+        fh.writelines("tar cvf " + workDir + "/" + outName + "/" + outName + "_fastq.tar \\\n")
+        fh.writelines("-C " + workDir + "/" + outName + " \\\n")
+        fh.writelines(outName + "_1.fastq.gz \\\n")
+        fh.writelines(outName + "_2.fastq.gz \n")
         fh.writelines("\n")
         fh.writelines("# Run Star Align \n")
         fh.writelines("\n")
         fh.writelines("python " + workDir + "/Resources/icgc_rnaseq_align/star_align.py \\\n")
         fh.writelines("--genomeDir " + workDir + "/Resources/Index/star_genome_d1_vd1_gtfv22 \\\n")
-        fh.writelines("--tarFileIn " + workDir + "/" + outName + "/" + outName + "_TEMP_fastq.tar \\\n")
+        fh.writelines("--tarFileIn " + workDir + "/" + outName + "/" + outName + "_fastq.tar \\\n")
         fh.writelines("--workDir " + workDir + "/" + outName + " \\\n")
         fh.writelines("--genomeFastaFiles " + workDir + "/Resources/Genome/GRCh38.d1.vd1.fa \\\n")
         fh.writelines("--out " + workDir + "/" + outName + "/" + outName + ".bam \\\n")
@@ -113,13 +112,14 @@ with open(job_file,"w") as fh:
         fh.writelines("\n")
         fh.writelines("source deactivate\n")
         fh.writelines("\n")
-        fh.writelines("rm " + workDir + "/" + outName + "/" + outName + "_TEMP_fastq.tar\n")
+	fh.writelines("rm " + workDir + "/" + outName + "/" + outName + "_fastq.tar \n")
         fh.writelines("\n")
         fh.writelines("samtools index " +  workDir + "/" + outName + "/" + outName + ".bam " +  workDir + "/" + outName + "/" + outName + ".bai")
+        fh.writelines("\n")
 
 fh.close()
 
-# Submit Slurm script
+# Submit SLURM script
 
 os.system("sbatch " + job_file)
 
